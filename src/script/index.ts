1b9c36ca12e7440295029d4dc9f2a20bc964d449
Nastya_Gavrilova <gavrilovan61@gmail.com>
2020-11-08 22:57:27 +0200

    hometask

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,3 +1,6 @@
 const $like = document.querySelector("#like");
 if($like instanceof HTMLElement){
 $like.addEventListener("click", () => ($like.style.color = "red"));}
+
+var year = "Year: " + new Date();
+document.getElementById("date").innerHTML = year
